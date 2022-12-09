6cc431bc2a0e91de77f33b75924f9a228e312207
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-12-09 08:27:56 +0100

    first commit

---
+++ data/data.js
@@ -0,0 +1,20 @@
+const comparisonData = {
+    ledLamp: {
+        startSentence: "Daarmee kun je een led lamp ",
+        consumptionPerGram: 0.5,
+        unit: "uur",
+        endSentence: " laten branden."
+    },
+    airplane: {
+        startSentence: "Daarmee kun je met een vliegtuig ",
+        consumptionPerGram: 0.005,
+        unit: "uur",
+        endSentence: " vliegen."
+    },
+    shower: {
+        startSentence: "Daarmee kun je ",
+        consumptionPerGram: 0.012,
+        unit: "minuten",
+        endSentence: " douchen"
+    }
+}
